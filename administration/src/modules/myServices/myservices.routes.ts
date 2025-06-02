9ed7fc46b33c2ca922b1dd4a4fbe25e9627370fd
TANHEHANZ <hantach10@gmail.com>
2025-06-02 16:11:16 -0400

    feat(routing):create routes  services

--- administration/src/modules/myServices/myservices.routes.ts
+++ administration/src/modules/myServices/myservices.routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { crudServiceController } from "./controllers/crud.controller";
+import { GroupController } from "./controllers/group.controller";
 
 const MyServices = Router();
 
-MyServices.get("/", crudServiceController.getAllServices);
+MyServices.get("/all", crudServiceController.getAllServices)
+  .post("/", crudServiceController.createService)
+  .put("/", crudServiceController.putServices)
+  .delete("/", crudServiceController.deletedServices);
+MyServices.get("/group", GroupController.AllGroups)
+  .post("/group", GroupController.createGroup)
+  .put("/group", GroupController.putServices);
 
 export default MyServices;
